06e643395e0de0764fabec180d89cf6b69304c1b
Jason Shipman <jasonpshipman@gmail.com>
2022-09-04 20:33:17 -0400

    WIP

--- otel-sdk-trace/library/OTel/SDK/Trace/Internal.hs
+++ otel-sdk-trace/library/OTel/SDK/Trace/Internal.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE UndecidableInstances #-}
 module OTel.SDK.Trace.Internal
   ( -- * Disclaimer
@@ -10,26 +14,48 @@ module OTel.SDK.Trace.Internal
   , forceFlushTracerProvider
 
   , SpanProcessor(..)
+  , buildSpanProcessor
   , simpleSpanProcessor
   , batchedSpanProcessor
+  , SpanProcessorSpec(..)
   ) where
 
-import Control.Exception.Safe (MonadMask)
+import Control.Exception (Exception(..))
+import Control.Exception.Safe (MonadCatch, MonadMask, catchAny)
+import Control.Monad ((<=<), void)
 import Control.Monad.IO.Class (MonadIO(liftIO))
-import Control.Monad.Logger (LoggingT(..), MonadLoggerIO(askLoggerIO))
-import Data.Function ((&))
-import OTel.API.Context (ContextBackend, withContextBackend)
-import OTel.API.Core
-  ( EndedSpan(..), SpanSpec(..), SpanStatus(..), Span, SpanAttrsLimits, SpanEventAttrsLimits
-  , SpanLinkAttrsLimits, Timestamp
+import Control.Monad.IO.Unlift (MonadUnliftIO(..))
+import Control.Monad.Logger.Aeson
+  ( LoggingT(..), Message(..), MonadLoggerIO(askLoggerIO), (.=), MonadLogger, SeriesElem, logError
   )
-import OTel.API.Core.Internal (MutableSpan(..), Span(..), Tracer(..), TracerProvider(..))
+import Control.Monad.Reader (ReaderT(..))
+import Data.Function ((&))
+import Data.Kind (Type)
+import Data.Monoid (Ap(..))
+import Data.Text (Text)
+import OTel.API.Context
+  ( ContextSnapshot(contextSnapshotValue), ContextT(runContextT), ContextBackend, getContext
+  , updateContext, withContextBackend
+  )
+import OTel.API.Core
+  ( EndedSpan(..), InstrumentationScope(..), InstrumentationScopeName(..)
+  , Version(..), SpanSpec(..), SpanStatus(..), Span, SpanAttrsLimits
+  , SpanEventAttrsLimits, SpanLinkAttrsLimits, Timestamp, UpdateSpanSpec, schemaURLToText, OpentracingRefType (OpentracingRefTypeChildOf)
+  )
+import OTel.API.Core.Internal
+  ( InstrumentationScope(InstrumentationScope), MutableSpan(..), Span(..), Tracer(..)
+  , TracerProvider(..), buildSpanUpdater
+  )
 import Prelude
+import System.Timeout (timeout)
 import qualified Control.Exception.Safe as Exception
+import Data.IORef (IORef, readIORef, writeIORef)
+import Control.Concurrent (MVar)
 
 data TracerProviderSpec = TracerProviderSpec
-  { tracerProviderSpecNow :: IO Timestamp
-  , tracerProviderSpecSpanProcessor :: SpanProcessor
+  { tracerProviderSpecInstrumentationScope :: InstrumentationScope
+  , tracerProviderSpecNow :: IO Timestamp
+  , tracerProviderSpecSpanProcessor :: [SpanProcessorSpec]
   , tracerProviderSpecSpanAttrsLimits :: SpanAttrsLimits
   , tracerProviderSpecSpanEventAttrsLimits :: SpanEventAttrsLimits
   , tracerProviderSpecSpanLinkAttrsLimits :: SpanLinkAttrsLimits
@@ -74,12 +100,20 @@ newTracerProvider ctxBackendTrace tracerProviderSpec = do
                 , spanEvents = mempty
                 , spanIsRecording = False
                 }
-          , tracerOnSpanStart = \mutableSpan ->
-              flip runLoggingT logger do
-                spanProcessorOnSpanStart spanProcessor mutableSpan
+          , tracerOnSpanStart = \case
+              MutableSpan { mutableSpanSpanKey = spanKey } -> do
+                spanProcessorOnSpanStart spanProcessor updateSpan getSpan
+                  & flip runLoggingT logger
+                where
+                getSpan :: LoggingT IO Span
+                getSpan = flip runContextT ctxBackendTrace do
+                  fmap contextSnapshotValue $ getContext spanKey
+                updateSpan :: UpdateSpanSpec -> LoggingT IO ()
+                updateSpan updatedSpan = flip runContextT ctxBackendTrace do
+                  void $ updateContext spanKey <=< buildSpanUpdater (liftIO now) $ updatedSpan
           , tracerOnSpanEnd = \endedSpan ->
-              flip runLoggingT logger do
-                spanProcessorOnSpanEnd spanProcessor endedSpan
+              spanProcessorOnSpanEnd spanProcessor endedSpan
+                & flip runLoggingT logger
           , tracerContextBackend = ctxBackendTrace
           , tracerSpanAttrsLimits = spanAttrsLimits
           , tracerSpanEventAttrsLimits = spanEventAttrsLimits
@@ -87,15 +121,38 @@ newTracerProvider ctxBackendTrace tracerProviderSpec = do
           }
     , tracerProviderShutdown =
         spanProcessorShutdown spanProcessor
+          & mkTimeoutSafe (mkLoggingMeta "shutdown")
+          & flip runTimeoutSafe 30000000 -- TODO: Make configurable?
           & flip runLoggingT logger
     , tracerProviderForceFlush =
         spanProcessorForceFlush spanProcessor
+          & mkTimeoutSafe (mkLoggingMeta "forceFlush")
+          & flip runTimeoutSafe 30000000 -- TODO: Make configurable?
           & flip runLoggingT logger
     }
- where
+  where
+  mkLoggingMeta :: Text -> [SeriesElem]
+  mkLoggingMeta method =
+    [ "instrumentationScopeName" .=
+        unInstrumentationScopeName instrumentationScopeName
+    , "instrumentationScopeVersion" .=
+        fmap unVersion instrumentationScopeVersion
+    , "instrumentationScopeSchemaURL" .=
+        fmap schemaURLToText instrumentationScopeSchemaURL
+    , "tracerProviderMethod" .= method
+    ]
+
+  spanProcessor = foldMap buildSpanProcessor spanProcessorSpecs
+
   TracerProviderSpec
-    { tracerProviderSpecNow = now
-    , tracerProviderSpecSpanProcessor = spanProcessor
+    { tracerProviderSpecInstrumentationScope =
+        InstrumentationScope
+          { instrumentationScopeName
+          , instrumentationScopeVersion
+          , instrumentationScopeSchemaURL
+          }
+    , tracerProviderSpecNow = now
+    , tracerProviderSpecSpanProcessor = spanProcessorSpecs
     , tracerProviderSpecSpanAttrsLimits = spanAttrsLimits
     , tracerProviderSpecSpanEventAttrsLimits = spanEventAttrsLimits
     , tracerProviderSpecSpanLinkAttrsLimits = spanLinkAttrsLimits
@@ -108,10 +165,17 @@ forceFlushTracerProvider :: forall m. (MonadIO m) => TracerProvider -> m ()
 forceFlushTracerProvider = liftIO . tracerProviderForceFlush
 
 data SpanProcessor = SpanProcessor
-  { spanProcessorOnSpanStart :: MutableSpan -> LoggingT IO ()
-  , spanProcessorOnSpanEnd :: EndedSpan -> LoggingT IO ()
-  , spanProcessorShutdown :: LoggingT IO ()
-  , spanProcessorForceFlush :: LoggingT IO ()
+  { spanProcessorOnSpanStart
+      :: (UpdateSpanSpec -> LoggingT IO ())
+      -> LoggingT IO Span
+      -> LoggingT IO ()
+  , spanProcessorOnSpanEnd
+      :: EndedSpan
+      -> LoggingT IO ()
+  , spanProcessorShutdown
+      :: LoggingT IO ()
+  , spanProcessorForceFlush
+      :: LoggingT IO ()
   }
 
 instance Semigroup SpanProcessor where
@@ -130,17 +194,158 @@ instance Semigroup SpanProcessor where
 instance Monoid SpanProcessor where
   mempty =
     SpanProcessor
-      { spanProcessorOnSpanStart = const $ pure mempty
+      { spanProcessorOnSpanStart = const $ const $ pure mempty
       , spanProcessorOnSpanEnd = const $ pure mempty
       , spanProcessorShutdown = pure mempty
       , spanProcessorForceFlush = pure mempty
       }
+
+buildSpanProcessor :: SpanProcessorSpec -> SpanProcessor
+buildSpanProcessor spanProcessorSpec = spanProcessor
+  where
+  spanProcessor =
+    SpanProcessor
+      { spanProcessorOnSpanStart = \updateSpan getSpan -> do
+          let loggingMeta = mkLoggingMeta "onSpanStart"
+          spanProcessorSpecOnSpanStart spanProcessorSpec updateSpan getSpan
+            & mkExceptionSafe loggingMeta
+            & runExceptionSafe
+      , spanProcessorOnSpanEnd = \endedSpan -> do
+          let loggingMeta = mkLoggingMeta "onSpanEnd"
+          spanProcessorSpecOnSpanEnd spanProcessorSpec endedSpan
+            & mkExceptionSafe loggingMeta
+            & runExceptionSafe
+      , spanProcessorShutdown = do
+          let loggingMeta = mkLoggingMeta "shutdown"
+          spanProcessorSpecShutdown spanProcessorSpec
+            & mkExceptionSafe loggingMeta
+            & mkTimeoutSafe loggingMeta
+            & flip runTimeoutSafe 30000000 -- TODO: Make configurable?
+            & runExceptionSafe
+      , spanProcessorForceFlush = do
+          let loggingMeta = mkLoggingMeta "forceFlush"
+          spanProcessorSpecForceFlush spanProcessorSpec
+            & mkExceptionSafe loggingMeta
+            & mkTimeoutSafe loggingMeta
+            & flip runTimeoutSafe 30000000 -- TODO: Make configurable?
+            & runExceptionSafe
+      }
+
+  mkLoggingMeta :: Text -> [SeriesElem]
+  mkLoggingMeta method =
+    [ "spanProcessorName" .= spanProcessorSpecName spanProcessorSpec
+    , "spanProcessorMethod" .= method
+    ]
 
 simpleSpanProcessor :: SpanProcessor
 simpleSpanProcessor = mempty -- TODO: Implement!
 
 batchedSpanProcessor :: SpanProcessor
 batchedSpanProcessor = mempty -- TODO: Implement!
+
+data SpanProcessorSpec = SpanProcessorSpec
+  { spanProcessorSpecName
+      :: Text
+  , spanProcessorSpecOnSpanStart
+      :: (UpdateSpanSpec -> LoggingT IO ())
+      -> LoggingT IO Span
+      -> LoggingT IO ()
+  , spanProcessorSpecOnSpanEnd
+      :: EndedSpan
+      -> LoggingT IO ()
+  , spanProcessorSpecShutdown
+      :: LoggingT IO ()
+  , spanProcessorSpecShutdownTimeout
+      :: Int
+  , spanProcessorSpecForceFlush
+      :: LoggingT IO ()
+  , spanProcessorSpecForceFlushTimeout
+      :: Int
+  }
+
+type ExceptionSafe :: (Type -> Type) -> Type -> Type
+newtype ExceptionSafe m a = ExceptionSafe
+  { runExceptionSafe :: m a
+  } deriving
+      ( Applicative, Functor, Monad, MonadIO -- @base@
+      , MonadUnliftIO -- @unliftio-core@
+      , MonadLogger -- @monad-logger@
+      ) via m
+    deriving
+      ( Semigroup, Monoid -- @base@
+      ) via (Ap m a)
+
+mkExceptionSafe
+  :: forall m
+   . (MonadCatch m, MonadLogger m)
+  => [SeriesElem]
+  -> m ()
+  -> ExceptionSafe m ()
+mkExceptionSafe pairs action =
+  ExceptionSafe do
+    action `catchAny` \ex -> do
+      logError $ "Ignoring exception" :#
+        ("exception" .= displayException ex) : pairs
+
+type TimeoutSafe :: (Type -> Type) -> Type -> Type
+newtype TimeoutSafe m a = TimeoutSafe
+  { runTimeoutSafe :: Int -> m a
+  } deriving
+      ( Applicative, Functor, Monad, MonadIO -- @base@
+      , MonadUnliftIO -- @unliftio-core@
+      , MonadLogger -- @monad-logger@
+      ) via (ReaderT Int m)
+    deriving
+      ( Semigroup, Monoid -- @base@
+      ) via (Ap (ReaderT Int m) a)
+
+mkTimeoutSafe
+  :: forall m
+   . (MonadLogger m, MonadUnliftIO m)
+  => [SeriesElem] -- ^ Metadata to log if action times out
+  -> m ()
+  -> TimeoutSafe m ()
+mkTimeoutSafe pairs action =
+  TimeoutSafe \microseconds -> do
+    mResult <- withRunInIO \runInIO -> do
+      timeout microseconds $ runInIO action
+    case mResult of
+      Just () -> pure ()
+      Nothing -> do
+        logError $ "Action did not complete within timeout" :#
+          ("timeoutMicroseconds" .= microseconds) : pairs
+
+type Once :: (Type -> Type) -> Type -> Type
+newtype Once m a = Once
+  { runOnce :: IORef Bool -> m a
+  } deriving
+      ( Applicative, Functor, Monad, MonadIO -- @base@
+      , MonadUnliftIO -- @unliftio-core@
+      , MonadLogger -- @monad-logger@
+      ) via (ReaderT (IORef Bool) m)
+    deriving
+      ( Semigroup, Monoid -- @base@
+      ) via (Ap (ReaderT (IORef Bool) m) a)
+
+mkOnce
+  :: forall m
+   . (MonadCatch m, MonadLogger m, MonadIO m)
+  => m ()
+  -> Once m ()
+mkOnce action =
+  Once \ref -> do
+    liftIO (readIORef ref) >>= \case
+      False -> do
+        wrappedAction
+        liftIO $ writeIORef ref True
+      True -> pure ()
+  where
+  wrappedAction = runExceptionSafe $ mkExceptionSafe [] action
+
+--runExactlyOnce :: forall m a. (MonadIO m) => m a -> m a
+--runExactlyOnce action = do
+--  ref <- liftIO $ newIORef Nothing
+--  action
 
 -- $disclaimer
 --
