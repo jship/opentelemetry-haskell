c489560e7257c7ceab44ad4bc74fe67117ee860c
Jason Shipman <jasonpshipman@gmail.com>
2023-04-04 16:37:02 -0400

    Re-export OTel.API.Common.Logging from OTel.API.Common

--- otel-api-common/library/OTel/API/Common.hs
+++ otel-api-common/library/OTel/API/Common.hs
@@ -5,6 +5,7 @@ module OTel.API.Common
   , module OTel.API.Common.CPS
   , module OTel.API.Common.Handlers
   , module OTel.API.Common.InstrumentationScope
+  , module OTel.API.Common.Logging
   , module OTel.API.Common.KV
   , module OTel.API.Common.Timestamp
   ) where
@@ -13,6 +14,7 @@ import OTel.API.Common.Attributes
 import OTel.API.Common.CPS
 import OTel.API.Common.Handlers
 import OTel.API.Common.InstrumentationScope
+import OTel.API.Common.Logging
 import OTel.API.Common.KV
 import OTel.API.Common.Timestamp
 
